f21f593b2c428cd90189ae6da54c83ad554d2f90
Gabor Szabo <szabogabi@gmail.com>
2020-01-22 12:21:32 +0100

    postthumb

--- templates/postcard.php
+++ templates/postcard.php
@@ -1,7 +1,11 @@
 <article <?php post_class('postcard'); ?>>
     <figure>
         <a href="<?php the_permalink(); ?>">
+            <?php if (has_post_thumbnail()) :?>
+                <?php the_post_thumbnail('medium43'); ?>
+            <?php else: ?>
             <img width="768" height="480" src="https://source.unsplash.com/768x480/?truck,lorry" alt="">
+            <?php endif; ?>
         </a>
     </figure>
     <h2 class="postcard__title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
